d3829119c8f2a3ae0dd5111612608c6159bf0d01
TivvSas <ozayasl0pec0010jk@gmail.com>
2026-05-06 22:38:35 -0400

    Version Base

---
+++ tecnico-api/tecnico-api/src/test/java/com/technoservice/tecnico_api/TecnicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technoservice.tecnico_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
